509e2992abfe00a7df32639249f24e1d46718381
유의선 <hisbrjseka@gmail.com>
2022-08-26 10:39:00 +0900

    AOP

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -24,10 +24,9 @@ public class MemberService {
      */
     public Long join(Member member){
 
-        validateDuplicateMember(member); // 중복 회원 검증
-
-        memberRepository.save(member);
-        return member.getId();
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
     }
 
     private void validateDuplicateMember(Member member) {
@@ -41,7 +40,9 @@ public class MemberService {
      * 전체 회원 조회
      */
     public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+            return memberRepository.findAll();
+
     }
 
     public Optional<Member> findOne(Long memberId){
